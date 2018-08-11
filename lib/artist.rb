ffce80ea4c6050f50fa0a46b4035e1c903343029
github username <github email address>
2018-08-11 14:06:19 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+require "pry"
+class Artist
+
+attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+## song order (name, artist, genre)
+def new_song(name, genre)
+  Song.new(name, self, genre)
+end
+
+def songs
+  Song.all.select do |song|
+    song.artist == self
+  end
+end
+
+def genres
+  songs.map do |song|
+    song.genre
+  end
+end
+
+# binding.pry
+
+
+end #end of class
